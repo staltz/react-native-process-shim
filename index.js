f48919620fd6c97032b912520aa0818166bdee87
Andre Staltz <andre@staltz.com>
2017-11-16 20:16:42 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var RNOS = require("react-native-os");
+module.exports = global.process = {
+  version: "7.1.0",
+  umask: () => 18,
+  cwd: () => "",
+  nextTick: setImmediate,
+  env: {
+    HOME: RNOS.homedir()
+  },
+  argv: ["react-native", "run-android", "your-app"],
+  versions: {}
+};
